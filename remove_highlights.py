39e1636d047487bb326bf6784ca87f6d5c7d0bd7
sahandtabtab <ss4tabat@uwaterloo.ca>
2022-08-06 23:23:14 -0400

    testing with regex

--- remove_highlights.py
+++ remove_highlights.py
@@ -1,16 +1,51 @@
-# This file partly uses code from https://stackoverflow.com/a/10477490/13055043 by Eli Bendersky
+# Requires the 'regex' or 're' module.
+# This file partly uses code from https://stackoverflow.com/a/10477490/13055043 by Eli Bendersky.
 
-import re
+from fileinput import filename
+import regex as re
 
-initials = input("Enter initials to remove, separated by spaces:\n").split()
+def get_highlights(commenter_initials, text):
+    stuff=""
+    count=0
+    for char in text:
+        if char== fr'\{commenter_initials}{{':
+            count += 1
+        if count > 0:
+            stuff += char
+        if char=="}":
+            count -= 1
+        if count == 0 and stuff != "":
+            yield stuff
+            stuff=""
 
-pattern_string = ''
-for initial in initials:
-    pattern_string += fr'\\{initial}|'
-pattern_string = pattern_string[:-1]
 
-pattern = re.compile(pattern_string)
+file_name = input("\nEnter file to be modified (you can just press enter if it's 'main.tex')\n")
+if file_name == '':
+    file_name = 'main.tex'
+print(f'\nModifying {file_name}\n')
 
-for i, line in enumerate(open('main.tex')):
-    for match in re.finditer(pattern, line):
-        print ('Found on line %s: %s' % (i+1, match.group()))
+initials = input("Enter initials to remove, separated by spaces:\n  E.g. ST RB HH\n\n").split()
+
+print('\n')
+
+print(list(
+    get_highlights("ST", "\ST{dasd}{ad} asd \HH{das}{vc}vc")
+))
+
+# for i, line in enumerate(open(file_name)):
+#     for comment in get_highlights(initials[0], line):
+#         print(f'Line: {i}')
+#         print(comment)
+
+
+# pattern_string = ''
+# for initial in initials:
+#     pattern_string += fr'\\{initial}' + r'( \{ ( (?: [^}{]+ | (?1))*+ ) \} )'#+ r'\{([^}]+)\}|'
+# pattern_string = pattern_string[:-1]
+
+# pattern = re.compile(pattern_string)
+
+# for i, line in enumerate(open(file_name)):
+#     for match in re.finditer(pattern, line):
+#         print(f'Line {i}:')
+#         print(match.group())
