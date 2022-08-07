954a9b50602c39cb23c760e0ee2c0915c2fa6ffa
sahandtabtab <ss4tabat@uwaterloo.ca>
2022-08-07 01:07:52 -0400

    Works better

--- remove_highlights.py
+++ remove_highlights.py
@@ -1,23 +1,49 @@
-# Requires the 'regex' or 're' module.
-# This file partly uses code from https://stackoverflow.com/a/10477490/13055043 by Eli Bendersky.
-
 from fileinput import filename
-import regex as re
 
-def get_highlights(commenter_initials, text):
-    stuff=""
-    count=0
-    for char in text:
-        if char== fr'\{commenter_initials}{{':
-            count += 1
-        if count > 0:
-            stuff += char
-        if char=="}":
-            count -= 1
-        if count == 0 and stuff != "":
-            yield stuff
-            stuff=""
+def remove_single_comment(string, key, change = {}):
 
+    key_word_length = len(key)
+
+    index = string.find(key)
+
+    if index == -1:
+        return string, change, index
+    else:
+        count = 0
+        i = 0
+        while (count != 0) | (i == 0):
+            if string[index + key_word_length + i] == r'{':
+                count += 1
+            elif string[index + key_word_length + i] == r'}':
+                count -= 1
+            i += 1
+
+        count = 0
+        j = 0
+        while (count != 0) | (j == 0):
+            if string[index + key_word_length + i + j] == r'{':
+                count += 1
+            elif string[index + key_word_length + i + j] == r'}':
+                count -= 1
+            j += 1
+
+        string_before_comment = string[:index]
+        mid_string = string[index + key_word_length + i + 1: index + key_word_length + i + j - 1]
+        string_after_comment = string[index + key_word_length + i + j:]
+        new_string = string_before_comment + mid_string + string_after_comment
+
+        change = {string[index:index + key_word_length + i + j]:mid_string}
+
+        return new_string, change, index
+
+def remove_comments(string, key):
+    index = 0
+    new_string = string
+    changes = {}
+    while index != -1:
+        new_string, new_change, index = remove_single_comment(new_string, key, changes)
+        changes = changes | new_change
+    return new_string, changes
 
 file_name = input("\nEnter file to be modified (you can just press enter if it's 'main.tex')\n")
 if file_name == '':
@@ -28,24 +54,20 @@ initials = input("Enter initials to remove, separated by spaces:\n  E.g. ST RB H
 
 print('\n')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 print(list(
     get_highlights("ST", "\ST{dasd}{ad} asd \HH{das}{vc}vc")
 ))
-
-# for i, line in enumerate(open(file_name)):
-#     for comment in get_highlights(initials[0], line):
-#         print(f'Line: {i}')
-#         print(comment)
-
-
-# pattern_string = ''
-# for initial in initials:
-#     pattern_string += fr'\\{initial}' + r'( \{ ( (?: [^}{]+ | (?1))*+ ) \} )'#+ r'\{([^}]+)\}|'
-# pattern_string = pattern_string[:-1]
-
-# pattern = re.compile(pattern_string)
-
-# for i, line in enumerate(open(file_name)):
-#     for match in re.finditer(pattern, line):
-#         print(f'Line {i}:')
-#         print(match.group())
